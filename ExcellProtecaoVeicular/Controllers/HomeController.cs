cd6ecdbdbb84a7cd6ce1a8ba0c14d50a4d213228
Brendon Genssinger Almeida Mascarenhas <brendon_genssinger@hotmail.com>
2017-02-27 13:26:16 -0300

    Criamos os arquivos css, js e páginas parciais(Partial view)

---
+++ ExcellProtecaoVeicular/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExcellProtecaoVeicular.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult _HeaderLayout()
+        {
+            return View();
+        }
+        public ActionResult _HeaderNavBarMenu()
+        {
+            return View();
+        }
+        public ActionResult Associacao()
+        {
+            return View();
+        }
+    }
+}
