dfed2e20aa7fa19fca80e0980e36c270eb783c90
Brendon Genssinger Almeida Mascarenhas <brendon_genssinger@hotmail.com>
2017-02-28 16:32:45 -0300

    Ajustando a parte de E-mail

--- ExcellProtecaoVeicular/Controllers/HomeController.cs
+++ ExcellProtecaoVeicular/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ExcellProtecaoVeicular.Models;
 
 namespace ExcellProtecaoVeicular.Controllers
 {
@@ -34,9 +35,38 @@ namespace ExcellProtecaoVeicular.Controllers
         {
             return View();
         }
+
+        
         public ActionResult Contato()
         {
+
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult Contato(_Email _objEmail)
+        {
+            if(ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add("brendon.genssinger@gmail.com");
+                mail.From = new MailAddress(_objEmail.Email);
+                mail.Subject = _objEmail.Nome;
+                mail.Body = @"<p>Testando E-amil<\p>";
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("brendon.genssinger@gmail.com", "1311261425");
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("Index", _objEmail);
+            }
+            else
+            {
+                return View();
+            }
         }
         
     }
---
+++ ExcellProtecaoVeicular/Models/_Email.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExcellProtecaoVeicular.Models
+{
+    public class _Email
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Mensagem { get; set; }
+        
+    }
+}
